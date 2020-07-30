96cc0a2bea038a5d1b6df7cd1ed228384881d308
mgienger <michael.gienger@honda-ri.de>
2020-07-30 18:03:03 +0200

    CMake log indentation adjusted

--- utils/cmake/EventValidation.cmake
+++ utils/cmake/EventValidation.cmake
@@ -15,7 +15,7 @@ else()
     option(ESLIB_VALIDATE_BLAME "Include git blame info in event validation errors" ON)
 
     if (ESLIB_VALIDATE_EVENTS)
-        message(STATUS " -- ESLib Event Validation ON")
+        message(STATUS "ESLib Event Validation ON")
 
         # copy event visualization tools to build directory for convenience
         file(COPY
@@ -27,12 +27,12 @@ else()
         endif()
 
         if (ESLIB_VALIDATE_BLAME)
-            message(STATUS " -- ESLib Event Validation will use git blame")
+            message(STATUS "ESLib Event Validation will use git blame")
         else()
-            message(STATUS " -- ESLib Event Validation will not use git blame")
+            message(STATUS "ESLib Event Validation will not use git blame")
         endif()
     else()
-        message(STATUS " -- ESLib Event Validation is DISABLED")
+        message(STATUS "ESLib Event Validation is DISABLED")
     endif()
 
     function(target_link_libraries _target)
